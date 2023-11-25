ea8310eb5346c8bc2e40d514b48113e9ae407e37
Shabin-k <helloshabink@gmail.com>
2023-11-25 21:27:34 +0530

    fixed

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
